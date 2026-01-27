ee2cbfa17840413a4b1c856485f3c705b455070d
raymundmt <mon2x0@gmail.com>
2026-01-27 13:57:36 +1300

    feat: finish Muninn setup with dynamic domain config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,13 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Muninn",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null, // Disabled
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: `wiki.${process.env.DOMAIN_NAME}`,
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -88,8 +86,8 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      // Commented out to speed up build time
+      // Plugin.CustomOgImages(),
     ],
   },
 }
